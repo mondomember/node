98b04a486a3bca76aba581b8485ea9cfa6001be0
Nate Iler <nate@flipboxdigital.com>
2022-04-27 09:11:23 -0600

    fix: test ids should be alpha-numeric

--- packages/test/lib/utils/ids.ts
+++ packages/test/lib/utils/ids.ts
@@ -1,7 +1,7 @@
 export function generateTestKSUID(prefix: string) {
-  return `${prefix}_${
-    Math.random().toString(36).substring(2, 10) +
-    Math.random().toString(36).substring(0, 11) +
-    Math.random().toString(36).substring(2, 10)
-  }`;
+  return `${prefix}_${Math.random()
+    .toString(36)
+    .substring(2, 10)}${Math.random()
+    .toString(36)
+    .substring(2, 15)}${Math.random().toString(36).substring(2, 10)}`;
 }
