31ce3a4a0240e3fc98332a2aa6969d2f14023662
Nate Iler <nate@flipboxdigital.com>
2022-05-30 13:36:09 -0600

    feat: adding workspace pagination parms to list

--- packages/sdk/lib/services/account/resources/workspace/models/response-list.ts
+++ packages/sdk/lib/services/account/resources/workspace/models/response-list.ts
@@ -1,5 +1,9 @@
 import { JsonSchemaType } from "../../../../../schema";
 import {
+  PaginationPropertySchema,
+  PaginationPropertyInterface,
+} from "../../../../../models";
+import {
   WorkspaceTenantResponseItemSchema,
   WorkspaceTenantResponseItemInterface,
 } from "./response-item";
@@ -13,9 +17,11 @@ export const WorkspaceTenantResponseListSchema = {
       type: JsonSchemaType.ARRAY,
       items: WorkspaceTenantResponseItemSchema,
     },
+    ...PaginationPropertySchema,
   },
 };
 
-export interface WorkspaceTenantResponseListInterface {
+export interface WorkspaceTenantResponseListInterface
+  extends Partial<PaginationPropertyInterface> {
   items?: WorkspaceTenantResponseItemInterface[];
 }
