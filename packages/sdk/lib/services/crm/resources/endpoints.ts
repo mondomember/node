44becb2402c13c5b7a3c46156cdc015f4100b17f
Nate Iler <nate@flipboxdigital.com>
2022-08-05 13:57:39 -0600

    feat: adding external lookup resource for customers

--- packages/sdk/lib/services/crm/resources/endpoints.ts
+++ packages/sdk/lib/services/crm/resources/endpoints.ts
@@ -1,6 +1,7 @@
 import Companies from "./companies/endpoints";
 import Contacts from "./contacts/endpoints";
 import CompanyContacts from "./company-contacts/endpoints";
+import External from "./external";
 
 import { ClientInstance } from "../../../client/interfaces";
 
@@ -8,10 +9,12 @@ export class CRMEndpoints {
   readonly Companies: Companies;
   readonly Contacts: Contacts;
   readonly CompanyContacts: CompanyContacts;
+  readonly External: External;
 
   constructor(readonly client: ClientInstance) {
     this.Companies = new Companies(this.client);
     this.Contacts = new Contacts(this.client);
     this.CompanyContacts = new CompanyContacts(this.client);
+    this.External = new External(this.client);
   }
 }
