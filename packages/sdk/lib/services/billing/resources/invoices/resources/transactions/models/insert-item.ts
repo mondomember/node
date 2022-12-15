658a8d825c500d88bb697511fd7a130953af39cb
Nate Iler <nate@flipboxdigital.com>
2022-12-15 09:32:30 -0700

    feat: enabling charge/refund transaction creation

--- packages/sdk/lib/services/billing/resources/invoices/resources/transactions/models/insert-item.ts
+++ packages/sdk/lib/services/billing/resources/invoices/resources/transactions/models/insert-item.ts
@@ -1,16 +1,6 @@
 import { JsonSchemaType, JsonSchema } from "../../../../../../../schema";
 import { InvoiceTransactionType } from "./interfaces";
 
-const TypeProperty = {
-  type: {
-    type: JsonSchemaType.STRING,
-    enum: [
-      InvoiceTransactionType.MANUAL_REFUND,
-      InvoiceTransactionType.ADJUSTMENT,
-    ],
-  },
-};
-
 const AmountProperty = {
   amount: {
     type: JsonSchemaType.NUMBER,
@@ -23,20 +13,89 @@ const NotesProperty = {
   },
 };
 
-export const InvoiceTransactionInsertItemSchema: JsonSchema = {
+const InvoiceChargeRefundTransactionInsertItemSchema: JsonSchema = {
   type: JsonSchemaType.OBJECT,
   additionalProperties: false,
-  required: ["type", "amount"],
+  required: ["type", "charge"],
   properties: {
-    ...TypeProperty,
+    type: {
+      type: JsonSchemaType.STRING,
+      enum: [InvoiceTransactionType.CHARGE_REFUND],
+    },
+    charge: {
+      type: JsonSchemaType.STRING,
+    },
     ...AmountProperty,
     ...NotesProperty,
   },
 };
 
+const InvoiceChargeTransactionInsertItemSchema: JsonSchema = {
+  type: JsonSchemaType.OBJECT,
+  additionalProperties: false,
+  required: ["type"],
+  properties: {
+    type: {
+      type: JsonSchemaType.STRING,
+      enum: [InvoiceTransactionType.CHARGE],
+    },
+    ...NotesProperty,
+  },
+};
+
+const InvoiceManualRefundTransactionInsertItemSchema: JsonSchema = {
+  type: JsonSchemaType.OBJECT,
+  additionalProperties: false,
+  required: ["type"],
+  properties: {
+    type: {
+      type: JsonSchemaType.STRING,
+      enum: [InvoiceTransactionType.MANUAL_REFUND],
+    },
+    ...AmountProperty,
+    ...NotesProperty,
+  },
+};
+
+const InvoiceAdjustmentTransactionInsertItemSchema: JsonSchema = {
+  type: JsonSchemaType.OBJECT,
+  additionalProperties: false,
+  required: ["type", "amount"],
+  properties: {
+    type: {
+      type: JsonSchemaType.STRING,
+      enum: [InvoiceTransactionType.ADJUSTMENT],
+    },
+    ...AmountProperty,
+    ...NotesProperty,
+  },
+};
+
+export const InvoiceTransactionInsertItemSchema: JsonSchema = {
+  type: JsonSchemaType.OBJECT,
+  oneOf: [
+    InvoiceChargeRefundTransactionInsertItemSchema,
+    InvoiceChargeTransactionInsertItemSchema,
+    InvoiceManualRefundTransactionInsertItemSchema,
+    InvoiceAdjustmentTransactionInsertItemSchema,
+  ],
+};
+
+interface Charge {
+  type: typeof InvoiceTransactionType.CHARGE;
+  notes?: string;
+}
+
+interface ChargeRefund {
+  type: typeof InvoiceTransactionType.CHARGE_REFUND;
+  charge: string;
+  amount?: number;
+  notes?: string;
+}
+
 interface ManualRefund {
   type: typeof InvoiceTransactionType.MANUAL_REFUND;
-  amount: number;
+  amount?: number;
   notes?: string;
 }
 
@@ -46,4 +105,8 @@ interface Other {
   notes?: string;
 }
 
-export type InvoiceTransactionInsertItemInterface = ManualRefund | Other;
+export type InvoiceTransactionInsertItemInterface =
+  | Charge
+  | ChargeRefund
+  | ManualRefund
+  | Other;
